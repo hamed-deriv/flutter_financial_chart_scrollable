289731ba83d564026043068465abb91da105b156
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-05-26 11:18:35 +0800

    add candle stick model

---
+++ lib/candle_stick_model.dart
@@ -0,0 +1,13 @@
+class CandleStickModel {
+  CandleStickModel({
+    required this.open,
+    required this.close,
+    required this.high,
+    required this.low,
+  });
+
+  final double open;
+  final double close;
+  final double high;
+  final double low;
+}
